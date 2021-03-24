8495ff80e6798311ab9cfe74f3add5d3a24c2ae3
Bohomaz Pavel <bogi1598@gmail.com>
2021-03-24 10:29:14 +0200

    t

--- src/main/java/servlets/AdminServlet.java
+++ src/main/java/servlets/AdminServlet.java
@@ -9,21 +9,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class AdminServlet extends HttpServlet {
-
-    private static int admActive = 0;
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         if (request.getParameter("pass").equals("123456789")){
-            admActive = 1;
-//            request.getSession().setAttribute("active", admActive);
-            request.setAttribute("active", admActive);
             RequestDispatcher requestDispatcher = request.getRequestDispatcher("page/adm.jsp");
             requestDispatcher.forward(request, response);
         }
